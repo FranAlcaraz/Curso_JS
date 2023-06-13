06a01e0591fc9b9bc5bf4b5f907584beca221ed2
Francisco Alcaraz <fran.alcaraz@live.com>
2023-06-12 22:42:13 -0300

    inicio_final

---
+++ TERCERA ENTREGA/js/product.js
@@ -0,0 +1,12 @@
+let productos = [
+{ id:1, nombre:"Remera Azul", precio: 2500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:2, nombre:"Remera Negra", precio: 2500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:3, nombre:"Remera Rosa", precio: 2500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:4, nombre:"Remera roja", precio: 2500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:5, nombre:"Remera blanca", precio: 2500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:6, nombre:"Pantalon Gris", precio: 3500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:7, nombre:"Pantalon Jean", precio: 4500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:8, nombre:"Pollera Cuadros", precio: 3500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:9, nombre:"Gorra Roja", precio: 1500, img:"", marca : "", cantidad:"", categoria:""},
+{ id:10, nombre:"Buzo Negro Diseño", precio: 9000, img:"", marca : "", cantidad:"", categoria:""}]
+
