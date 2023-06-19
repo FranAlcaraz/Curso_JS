e036e747c961216705f4182a39b3d3a20c2c5395
Francisco Alcaraz <fran.alcaraz@live.com>
2023-06-19 18:38:40 -0300

    api pk

---
+++ PROYECTO FINAL/js/api.js
@@ -0,0 +1,68 @@
+const apiUrl = 'https://pokeapi.co/api/v2';
+
+// Obtener los primeros 151 Pokémon (generación 1)
+async function obtenerPokemonOriginales() {
+  try {
+    const response = await fetch(`${apiUrl}/pokemon?limit=151`);
+    const data = await response.json();
+    const pokemonList = data.results;
+
+    // Dividir la lista de Pokémon en grupos de 5
+    const gruposDePokemon = dividirEnGrupos(pokemonList, 5);
+
+    // Mostrar los Pokémon originales en el HTML
+    const pokemonContainer = document.getElementById('cuerpo2');
+    gruposDePokemon.forEach(async (grupo) => {
+      const pokemonPromises = grupo.map((pokemon) => obtenerPokemonDetalles(pokemon.url));
+      const pokemonsData = await Promise.all(pokemonPromises);
+      pokemonContainer.innerHTML += crearGrupoHTML(pokemonsData);
+    });
+  } catch (error) {
+    console.log('Error al obtener los Pokémon originales:', error);
+  }
+}
+
+// Obtener detalles de un Pokémon
+async function obtenerPokemonDetalles(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error al obtener los detalles del Pokémon:', error);
+  }
+}
+
+// Dividir un array en grupos de un tamaño específico
+function dividirEnGrupos(array, tamañoGrupo) {
+  const grupos = [];
+  for (let i = 0; i < array.length; i += tamañoGrupo) {
+    grupos.push(array.slice(i, i + tamañoGrupo));
+  }
+  return grupos;
+}
+
+// Crear el HTML para un grupo de Pokémon
+function crearGrupoHTML(pokemonDataArray) {
+  let grupoHTML = '<div class="grupo d-flex bd-highlight">';
+  pokemonDataArray.forEach((pokemonData) => {
+    const habilidades = pokemonData.abilities.map((ability) => ability.ability.name);
+    const tipos = pokemonData.types.map((type) => type.type.name);
+    grupoHTML += `
+      <div class="pokemon card">
+        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name} class='card-img-top'">
+        <h3 class='card-text'>${pokemonData.name}</h3>
+        <p class='card-text'>ID: ${pokemonData.id}</p>
+        <p class='card-text'>Altura: ${pokemonData.height}</p>
+        <p class='card-text'>Peso: ${pokemonData.weight}</p>
+        <p>Tipo: ${tipos.join(', ')}</p>
+        <p class='card-text'>Habilidades: ${habilidades.join(', ')}</p>
+      </div>
+    `;
+  });
+  grupoHTML += '</div>';
+  return grupoHTML;
+}
+
+// Llamar a la función para obtener los Pokémon originales
+obtenerPokemonOriginales();
--- PROYECTO FINAL/js/script1.js
+++ PROYECTO FINAL/js/script1.js
@@ -1,194 +1,147 @@
-
-//array para almacenar productos seleccionados
+// Array para almacenar productos seleccionados
 let carroDeCompras = [];
 
 const productosElegidos = document.getElementById('miCarroCompras');
-
 let bodyTable = document.getElementById("cuerpo");
-
 let botonCarrito = document.getElementById('abrirCarrito');
-
-const agregados = document.getElementById('itemAgregado')
-
+const agregados = document.getElementById('itemAgregado');
 const precioTotal = document.getElementById('precioTotal');
-
 let elegirPorqueFiltrar = document.querySelectorAll('input[type="radio"]');
-
 let ArrayFilter = [];
 
-elegirPorqueFiltrar.forEach((check) =>
-  check.addEventListener("change", handleChange),
-);
+elegirPorqueFiltrar.forEach((check) => check.addEventListener("change", handleChange));
 
 function handleChange() {
-    
   bodyTable.innerHTML = "";
+  const checkeds = Array.from(elegirPorqueFiltrar).filter((checkbox) => checkbox.checked);
+  const checkedValue = checkeds.map((checkbox) => checkbox.value);
 
-  
-  const checkeds = Array.from(elegirPorqueFiltrar).filter(
-    (checkbox) => checkbox.checked,
-    );
-    
-    
-    const checkedValue = checkeds.map((checkbox) => checkbox.value);
-    
-    
-
-  
   if (checkedValue == "all") {
-    
     ArrayFilter = productos;
   } else if (checkedValue == "pantalon") {
-    
-    ArrayFilter = productos.filter(
-      (producto) => producto.categoria == "pantalon",
-      );
-    } else if (checkedValue == "blusa") {
-    
-    ArrayFilter = productos.filter(
-      (producto) => producto.categoria == "blusa",
-    );
+    ArrayFilter = productos.filter((producto) => producto.categoria == "pantalon");
+  } else if (checkedValue == "blusa") {
+    ArrayFilter = productos.filter((producto) => producto.categoria == "blusa");
   } else if (checkedValue == "entero") {
-    
-    ArrayFilter = productos.filter(
-      (producto) => producto.categoria == "entero",
-      );
-    } else if (checkedValue == "top") {
-      
-      ArrayFilter = productos.filter((producto) => producto.categoria == "top");
-    } else if (checkedValue == "vestido") {
-      
-      ArrayFilter = productos.filter(
-        (producto) => producto.categoria == "vestido",
-        );
-      }
-      
-      
-      
-      completarTabla(ArrayFilter);
-    }
-    
-    completarTabla(productos); x
-    
-    function completarTabla(prod){
-      
-      
+    ArrayFilter = productos.filter((producto) => producto.categoria == "entero");
+  } else if (checkedValue == "top") {
+    ArrayFilter = productos.filter((producto) => producto.categoria == "top");
+  } else if (checkedValue == "vestido") {
+    ArrayFilter = productos.filter((producto) => producto.categoria == "vestido");
+  }
 
+  completarTabla(ArrayFilter);
+}
 
-  bodyTable.innerHTML='';
+completarTabla(productos); 
 
-  for(const producto of prod){
+function completarTabla(prod) {
+  bodyTable.innerHTML = '';
+
+  for (const producto of prod) {
     let div = document.createElement("div");
-    div.className="card";
-    div.innerHTML=`<img src=${producto.img} class='card-img-top'> <div class='card-body'>
-    <h5 class='card-title'>${producto.nombre} </h5> <p class='card-text'>$ ${producto.precio}</p>
-    <p class='card-text'>Stock: ${producto.stock}</p>
-    <button type="button" class="btn btn-primary" class="producto" id="producto_${producto.id}">Agregar al Carrito
-    </button></div>`
+    div.className = "card card-personalizada";
+    div.innerHTML = `<img src=${producto.img} class='card-img-top'> <div class='card-body'>
+      <h5 class='card-title'>${producto.nombre} </h5> <p class='card-text'>$ ${producto.precio}</p>
+      <p class='card-text'>Stock: ${producto.stock}</p>
+      <button type="button" class="btn btn-primary producto" id="producto_${producto.id}">Agregar al Carrito</button>
+    </div>`;
     bodyTable.appendChild(div);
 
-    
-    let botonAgregar = document.getElementById(`producto_${producto.id}`)
-    botonAgregar.addEventListener('click', ()=> {
+    let botonAgregar = document.getElementById(`producto_${producto.id}`);
+    botonAgregar.addEventListener('click', () => {
       console.log(producto.nombre);
       Toastify({
         text: `${producto.nombre} agregado a tu carrito con éxito`,
-          duration: 1000,
-          destination: "https://github.com/apvarun/toastify-js",
-          newWindow: true,
-          close: true,
-          gravity: "bottom", 
-          position: "right", 
-          stopOnFocus: true, 
-          style: {
-            background: "linear-gradient(to left,#6dadfa  , #2ac070 )",
-          },
-          onClick: function(){}
+        duration: 1000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true,
+        onClick: function(){}
       }).showToast();
 
-      agregarProducto(producto.id)
-    })
+      agregarProducto(producto.id);
+    });
   }
 }
 
+function agregarProducto(id) {
+  let agregar = carroDeCompras.find(item => item.id == id);
 
-function agregarProducto (id){
+  if (agregar) {
+    agregar.cantidad = agregar.cantidad + 1;
+    agregar.stock = agregar.stock - 1;
+    document.getElementById(`cantidad${agregar.id}`).innerHTML = `<p id=cantidad${agregar.id}> ${agregar.cantidad}</p>`;
+    actualizarCarrito();
+  } else {
+    let agregarProducto = productos.find(elem => elem.id == id);
+    carroDeCompras.push(agregarProducto);
+    actualizarCarrito();
+    console.log(carroDeCompras);
 
-  let agregar = carroDeCompras.find(item => item.id == id)
-
-  if (agregar){
-    agregar.cantidad = agregar.cantidad+1
-    agregar.stock = agregar.stock-1
-    console.log(agregar.cantidad)
-    console.log(agregar.stock)
-    console.log(document.getElementById(`cantidad${agregar.id}`))
-    document.getElementById(`cantidad${agregar.id}`).innerHTML = `<p id=cantidad${agregar.id}> Cantidad : ${agregar.cantidad}</p>`
-  
-  
-    actualizarCarrito()
-
-  }else{
-    let agregarProducto = productos.find(elem => elem.id == id)
-
-    carroDeCompras.push(agregarProducto)
-
-    actualizarCarrito()
-    console.log(carroDeCompras)
-
-    let div = document.createElement('div')
-    div.className = 'compra'
-
-    div.innerHTML=`<div class="contenedorProductos card">
-    <img src="${agregarProducto.img}" alt="" class="card-img-top">
-    <div class="textoPaquete card-body">
-    <h5 class="card-title">${agregarProducto.nombre}</h5>
+    let div = document.createElement('div');
+    div.className = 'compra';
+    div.innerHTML = `<div class="contenedorProductos card">
+      <img src="${agregarProducto.img}" alt="" class="card-img-top">
+      <div class="textoPaquete card-body">
+        <h5 class="card-title">${agregarProducto.nombre}</h5>
         cantidad: <span class="card-text" id=cantidad${agregarProducto.id}>${agregarProducto.cantidad}</span>
-    </div>
-    <p class="precio card-text">precio: ${agregarProducto.precio}</p>
-    <button id="eliminar${agregarProducto.id}" class="btn-eliminar btn btn-warning">eliminar</button>
-    <br>
-</div>`
+      </div>
+      <p class="precio card-text">precio: ${agregarProducto.precio}</p>
+      <button id="eliminar${agregarProducto.id}" class="btn-eliminar btn btn-warning">eliminar</button>
+      <br>
+    </div>`;
 
-productosElegidos.appendChild(div)
+    productosElegidos.appendChild(div);
 
-let borrar = document.getElementById(`eliminar${agregarProducto.id}`)
-
-borrar.addEventListener('click', ()=> {
-  console.log(agregarProducto.nombre + 'Producto eliminado');
-  Toastify({
-    text: ` ${agregarProducto.nombre} Eliminado de tu carrito`,
-                        duration: 1200,
-                        destination: "https://github.com/apvarun/toastify-js",
-                        newWindow: true,
-                        close: true,
-                        gravity: "top", 
-                        position: "right", 
-                        stopOnFocus: true, 
-                        style: {
-                          background: "linear-gradient(to left,#6dadfa  , #2ac070)",
-                          borderRadius:"20px",
-                        },
-                        onClick: function(){} 
-  }).showToast();
-  borrar.parentElement.remove()
-
-  carroDeCompras=carroDeCompras.filter(elem=>elem.id != agregarProducto.id)
-
-  actualizarCarrito()
-
-  localStorage.setItem('carrito',JSON.stringify(carroDeCompras))
-})
-
+    let borrar = document.getElementById(`eliminar${agregarProducto.id}`);
+    borrar.addEventListener('click', () => {
+      console.log(agregarProducto.nombre + 'Producto eliminado');
+      Toastify({
+        text: `${agregarProducto.nombre} Eliminado de tu carrito`,
+        duration: 1200,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true,
+        onClick: function(){}
+      }).showToast();
+      borrar.parentElement.remove();
+      carroDeCompras = carroDeCompras.filter(elem => elem.id != agregarProducto.id);
+      actualizarCarrito();
+      localStorage.setItem('carrito', JSON.stringify(carroDeCompras));
+    });
   }
 
-  localStorage.setItem('compraProducto', JSON.stringify(carroDeCompras))
+  localStorage.setItem('carrito', JSON.stringify(carroDeCompras));
+
 }
 
-function actualizarCarrito(){
-  // porfa reduce los agregados a un solo valor
-  agregados.innerHTML = carroDeCompras.reduce((acc,el) => acc + el.cantidad, 0)
-  precioTotal.innerText = carroDeCompras.reduce((acc,el)=> acc + (el.precio * el.cantidad), 0)
-  }
+function actualizarCarrito() {
+  const cantidadTotal = carroDeCompras.reduce((acc, el) => acc + el.cantidad, 0);
+  const precioTotalCarrito = carroDeCompras.reduce((acc, el) => acc + (el.precio * el.cantidad), 0);
+  agregados.innerHTML = cantidadTotal;
+  precioTotal.innerText = precioTotalCarrito;
+}
 
+
+function mantenerCarrito (){
+  let carritoStorage = JSON.parse(localStorage.getItem('carrito'))
   
-  
+  if(carritoStorage){
+    carritoStorage.forEach(element =>{
+          agregarProducto(element.id)
+
+      });
+  }
+}
+mantenerCarrito();
+
+
+
+
