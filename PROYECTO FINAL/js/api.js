0de79439598d14801fbe1b4e6f7705407f8596cb
Francisco Alcaraz <fran.alcaraz@live.com>
2023-06-19 18:53:34 -0300

    subida final

--- PROYECTO FINAL/js/api.js
+++ PROYECTO FINAL/js/api.js
@@ -1,3 +1,4 @@
+
 const apiUrl = 'https://pokeapi.co/api/v2';
 
 // Obtener los primeros 151 Pokémon (generación 1)
@@ -15,7 +16,8 @@ async function obtenerPokemonOriginales() {
     gruposDePokemon.forEach(async (grupo) => {
       const pokemonPromises = grupo.map((pokemon) => obtenerPokemonDetalles(pokemon.url));
       const pokemonsData = await Promise.all(pokemonPromises);
-      pokemonContainer.innerHTML += crearGrupoHTML(pokemonsData);
+      const grupoHTML = crearGrupoHTML(pokemonsData);
+      pokemonContainer.innerHTML += grupoHTML;
     });
   } catch (error) {
     console.log('Error al obtener los Pokémon originales:', error);
@@ -44,21 +46,23 @@ function dividirEnGrupos(array, tamañoGrupo) {
 
 // Crear el HTML para un grupo de Pokémon
 function crearGrupoHTML(pokemonDataArray) {
-  let grupoHTML = '<div class="grupo d-flex bd-highlight">';
+  let grupoHTML = '<div class="grupo">';
   pokemonDataArray.forEach((pokemonData) => {
     const habilidades = pokemonData.abilities.map((ability) => ability.ability.name);
     const tipos = pokemonData.types.map((type) => type.type.name);
     grupoHTML += `
-      <div class="pokemon card">
-        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name} class='card-img-top'">
-        <h3 class='card-text'>${pokemonData.name}</h3>
-        <p class='card-text'>ID: ${pokemonData.id}</p>
-        <p class='card-text'>Altura: ${pokemonData.height}</p>
-        <p class='card-text'>Peso: ${pokemonData.weight}</p>
+    <div class="pokemon card">
+      <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="card-img-top">
+      <div class="card-body">
+        <h3 class="card-text">${pokemonData.name}</h3>
+        <p class="card-text">ID: ${pokemonData.id}</p>
+        <p class="card-text">Altura: ${pokemonData.height}</p>
+        <p class="card-text">Peso: ${pokemonData.weight}</p>
         <p>Tipo: ${tipos.join(', ')}</p>
-        <p class='card-text'>Habilidades: ${habilidades.join(', ')}</p>
+        <p class="card-text">Habilidades: ${habilidades.join(', ')}</p>
       </div>
-    `;
+    </div>
+  `;
   });
   grupoHTML += '</div>';
   return grupoHTML;
